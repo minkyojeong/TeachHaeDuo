2a459c8f360b4617f282302ae2c618e56e15a324
yuni991227 <sy4ee@naver.com>
2022-04-26 21:20:09 +0900

    AlarmVo 수정
    - 변수명 수정

--- TeachHaeDuo_src/teachHaeDuo/src/kh/semi/thduo/alarm/model/vo/AlarmVo.java
+++ TeachHaeDuo_src/teachHaeDuo/src/kh/semi/thduo/alarm/model/vo/AlarmVo.java
@@ -3,91 +3,58 @@ package kh.semi.thduo.alarm.model.vo;
 import java.sql.Timestamp;
 
 public class AlarmVo {
-	private int alarmNo;
-	private String alarmContent;
-	private Timestamp alarmDate;
-	private String alarmSendId;
-	private String alarmReceiveId;
-	private String mId;
-	
-	
-	public AlarmVo(int alarmNo, String alarmContent, Timestamp alarmDate, String alarmSendId, String alarmReceiveId,
-			String mId) {
-		super();
-		this.alarmNo = alarmNo;
-		this.alarmContent = alarmContent;
-		this.alarmDate = alarmDate;
-		this.alarmSendId = alarmSendId;
-		this.alarmReceiveId = alarmReceiveId;
-		this.mId = mId;
-	}
 
+	private int alarm_no;
+	private String alarm_content;
+	private Timestamp alarm_date;
+	private String alarm_sendid;
+	private String alarm_receiveid;
+	private String m_id;
+	
+	public AlarmVo() {
+		super();
+	}
 
 	@Override
 	public String toString() {
-		return "AlarmVo [alarmNo=" + alarmNo + ", alarmContent=" + alarmContent + ", alarmDate=" + alarmDate
-				+ ", alarmSendId=" + alarmSendId + ", alarmReceiveId=" + alarmReceiveId + ", mId=" + mId + "]";
-	}
-
-
-	public int getAlarmNo() {
-		return alarmNo;
-	}
-
-
-	public void setAlarmNo(int alarmNo) {
-		this.alarmNo = alarmNo;
-	}
-
-
-	public String getAlarmContent() {
-		return alarmContent;
-	}
-
-
-	public void setAlarmContent(String alarmContent) {
-		this.alarmContent = alarmContent;
-	}
-
-
-	public Timestamp getAlarmDate() {
-		return alarmDate;
-	}
-
-
-	public void setAlarmDate(Timestamp alarmDate) {
-		this.alarmDate = alarmDate;
-	}
-
-
-	public String getAlarmSendId() {
-		return alarmSendId;
-	}
-
-
-	public void setAlarmSendId(String alarmSendId) {
-		this.alarmSendId = alarmSendId;
-	}
-
-
-	public String getAlarmReceiveId() {
-		return alarmReceiveId;
-	}
-
-
-	public void setAlarmReceiveId(String alarmReceiveId) {
-		this.alarmReceiveId = alarmReceiveId;
-	}
-
-
-	public String getmId() {
-		return mId;
-	}
-
-
-	public void setmId(String mId) {
-		this.mId = mId;
+		return "AlarmVo [alarm_no=" + alarm_no + ", alarm_content=" + alarm_content + ", alarm_date=" + alarm_date
+				+ ", alarm_sendid=" + alarm_sendid + ", alarm_receiveid=" + alarm_receiveid + ", m_id=" + m_id + "]";
 	}
 	
-	
+	public int getAlarm_no() {
+		return alarm_no;
+	}
+	public void setAlarm_no(int alarm_no) {
+		this.alarm_no = alarm_no;
+	}
+	public String getAlarm_content() {
+		return alarm_content;
+	}
+	public void setAlarm_content(String alarm_content) {
+		this.alarm_content = alarm_content;
+	}
+	public Timestamp getAlarm_date() {
+		return alarm_date;
+	}
+	public void setAlarm_date(Timestamp alarm_date) {
+		this.alarm_date = alarm_date;
+	}
+	public String getAlarm_sendid() {
+		return alarm_sendid;
+	}
+	public void setAlarm_sendid(String alarm_sendid) {
+		this.alarm_sendid = alarm_sendid;
+	}
+	public String getAlarm_receiveid() {
+		return alarm_receiveid;
+	}
+	public void setAlarm_receiveid(String alarm_receiveid) {
+		this.alarm_receiveid = alarm_receiveid;
+	}
+	public String getM_id() {
+		return m_id;
+	}
+	public void setM_id(String m_id) {
+		this.m_id = m_id;
+	}
 }
