64a57cd28c6fe2747883b7b6e95836a014e598c1
minkyojeong <98323085+minkyojeong@users.noreply.github.com>
2022-05-04 04:31:51 +0900

    20220504 작업
    선생님 프로필 등록 수정
    (이미 있는 파일인 경우 삭제 작동하도록)

--- TeachHaeDuo_src/teachHaeDuo/src/kh/semi/thduo/mypage/controller/ProfileUpdateDoController.java
+++ TeachHaeDuo_src/teachHaeDuo/src/kh/semi/thduo/mypage/controller/ProfileUpdateDoController.java
@@ -23,17 +23,18 @@ import kh.semi.thduo.teacher.model.vo.TeacherVo;
 @WebServlet("/profileUpdate.do")
 public class ProfileUpdateDoController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ProfileUpdateDoController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public ProfileUpdateDoController() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
 //	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 //		// TODO Auto-generated method stub
@@ -41,11 +42,23 @@ public class ProfileUpdateDoController extends HttpServlet {
 //	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		System.out.println("프로필 사진 등록 doPost");
-		
+		String tNo = "";
+		TeacherVo tVo = null;
+		MemberVo ssMV = (MemberVo) request.getSession().getAttribute("ssMV");
+		if (ssMV == null) { // 로그아웃 상태라면 로그인 페이지로
+			response.sendRedirect("login");
+			return; // 아래 db 저장을 할 필요가 없으니..
+		} else {
+			tNo = ssMV.gettNo();
+			tVo = new TeacherService().readTeacherInfo(tNo);
+		}
+
 		System.out.println(request.getContextPath());
 		String fileSavePath = "uploadProfile";
 		String uploadPath = getServletContext().getRealPath(fileSavePath);
@@ -54,43 +67,41 @@ public class ProfileUpdateDoController extends HttpServlet {
 		System.out.println("rootPath: " + rootPath);
 		// 업로드 할 폴더 존재 여부 확인 - > 없다면 생성해줘야됨
 		File path = new File(uploadPath);
-		if(!path.exists()) {
+		if (!path.exists()) {
 			path.mkdirs();
 		}
-		
-		int maxFileSize = 10*1024*1024;
-		MultipartRequest multi = new MultipartRequest(request, uploadPath
-				, maxFileSize
-				, "UTF-8"
-				, new DefaultFileRenamePolicy()); // 받아온 파일(request)를 뒤에 경로에 저장하겠다 // 파일 업로드 완료
-	
-		
+
+		int maxFileSize = 10 * 1024 * 1024;
+		MultipartRequest multi = new MultipartRequest(request, uploadPath, maxFileSize, "UTF-8",
+				new DefaultFileRenamePolicy()); // 받아온 파일(request)를 뒤에 경로에 저장하겠다 // 파일 업로드 완료
+
 		// 구현중 TODOTODO
-		String pFilePathParam= multi.getParameter("bFilePath");
-//			
-		String orgFileName = multi.getOriginalFileName("uploadProfile");  // 전송되어오기전 client에서 파일이름
-		String type = multi.getContentType("uploadProfile");	// 전송된 파일의 타입
-		String upload = multi.getFilesystemName("uploadProfile");	// 서버에 저장된 파일이름
-		if(upload == null && orgFileName != null) { // 파일 저장 실패
-			
+		String pFilePathParam = tVo.getT_picture();
+		System.out.println("pFilePathParam:" + pFilePathParam);
+		String orgFileName = multi.getOriginalFileName("uploadProfile"); // 전송되어오기전 client에서 파일이름
+		String type = multi.getContentType("uploadProfile"); // 전송된 파일의 타입
+		String upload = multi.getFilesystemName("uploadProfile"); // 서버에 저장된 파일이름
+		if (upload == null && orgFileName != null) { // 파일 저장 실패
+
 			response.sendRedirect("mypage");
 			return;
 		}
 		String pFilePath = "";
-		if(upload != null) {
-			System.out.println("프로필 등록");
-			pFilePath = fileSavePath + "/" + upload;
-		} else if ( upload != null && pFilePathParam != null) {
+		if (upload != null && pFilePathParam != null) {
 			// 기존사진 있음 + 새사진이 있는경우
 			// 기존사진 서버에서 파일 삭제
-			File file = new File(rootPath+pFilePathParam);
-			if(file.exists()) { // 파일명까지 적었기 때문에, 파일 존재여부 확인
+			File file = new File(rootPath + pFilePathParam);
+			if (file.exists()) { // 파일명까지 적었기 때문에, 파일 존재여부 확인
 				file.delete();
 				System.out.println("파일삭제");
 			} // 파일 없다면.. 아무 행동하지 않고 db 저장하러 감
-			// 새 파일을 db에 저장
-			pFilePath = fileSavePath + "/" + upload;  // 새파일을 db에 저장
-		} 
+				// 새 파일을 db에 저장
+			pFilePath = fileSavePath + "/" + upload; // 새파일을 db에 저장
+
+		} else if (upload != null) {
+			System.out.println("프로필 등록");
+			pFilePath = fileSavePath + "/" + upload;
+		}
 //		System.out.println(bTitle);
 //		System.out.println(bContent);
 //		System.out.println("upload:" + upload);
@@ -101,29 +112,31 @@ public class ProfileUpdateDoController extends HttpServlet {
 //		
 //		
 //		BoardVo vo = new BoardVo();
-		TeacherVo tVo = new TeacherVo();
-		System.out.println("pFilePath: " + pFilePath);
 		
+		System.out.println("pFilePath: " + pFilePath);
+
 //		vo.setbNo(bNo);
 //		vo.setbContent(bContent);
 //		vo.setbTitle(bTitle); 
 //		System.out.println("bFilePath: " + bFilePath);
 //		vo.setbFilePath(bFilePath);
 //		// 글쓰기 버튼 누를때 로그인 상태 확인했는데, 이때도 또 해줘! 왜냐면 그 사이에 로그아웃 됐을수도 있자나
-		MemberVo ssMV = (MemberVo)request.getSession().getAttribute("ssMV");
-		if(ssMV == null) {  // 로그아웃 상태라면 로그인 페이지로
-			response.sendRedirect("login");
-			return;  // 아래 db 저장을 할 필요가 없으니..
-		} else {  // 로그인한 상태라면 write page 진입 
-			tVo.setT_no(ssMV.gettNo());
-			tVo.setT_picture(pFilePath);
-		}
+
+//		if (ssMV == null) { // 로그아웃 상태라면 로그인 페이지로
+//			response.sendRedirect("login");
+//			return; // 아래 db 저장을 할 필요가 없으니..
+//		} else { // 로그인한 상태라면 write page 진입
+//			tVo.setT_no(ssMV.gettNo());
+//			
+//		}
 //		
+		
 //		// db저장
 		int result = 0;
+		tVo.setT_picture(pFilePath);
 		result = new TeacherService().updateProfile(tVo);
 		
-		if(result == 1) {
+		if (result == 1) {
 			request.setAttribute("rootPath", rootPath);
 			request.setAttribute("tVo", tVo);
 			request.getSession().setAttribute("msgProfile", "프로필 변경이 완료되었습니다.");
