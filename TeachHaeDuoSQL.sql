58f0e61e2122816fed47b97cbd1cb9cf2c9372dc
iglove98 <98322180+iglove98@users.noreply.github.com>
2022-05-04 18:06:47 +0900

    고객센터 sequence 추가

--- TeachHaeDuoSQL.sql
+++ TeachHaeDuoSQL.sql
@@ -661,4 +661,12 @@ CREATE SEQUENCE b_r_no_sequence
        NOORDER;
        commit;
        
-       
+    CREATE SEQUENCE faq_no_sequence
+       INCREMENT BY 1
+       START WITH 1
+       MINVALUE 1
+       MAXVALUE 9999
+       NOCYCLE
+       NOCACHE
+       NOORDER;
+       commit;
